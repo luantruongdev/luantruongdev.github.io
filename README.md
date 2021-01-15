3afe97bd6dfbe339d36cd49f06589da2798d2f34
luantruongdev <truongnhatluan95@gmail.com>
2021-01-15 20:13:57 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# My Portfolio page
+
+
+
+
+## License
+**Creative Commons Attribution 3.0** - http://creativecommons.org/licenses/by/3.0/
+
+
+**Bug tracker**
+
+Found a bug? Please create an issue here on GitHub! 
+https://github.com/pozh/Initio/issues
+
+
+
+## Credits
+* Design and development: **Sergey Pozhilov** - http://pozhilov.com
+* More free templates by Sergey: http://gettemplate.com
+
---
+++ assets/js/template.js
@@ -0,0 +1,109 @@
+jQuery(document).ready(function ($) {
+
+	var my_nav = $('.navbar-sticky');
+	// grab the initial top offset of the navigation 
+	var sticky_navigation_offset_top = my_nav.offset().top;
+
+	// our function that decides weather the navigation bar should have "fixed" css position or not.
+	var sticky_navigation = function () {
+		var scroll_top = $(window).scrollTop(); // our current vertical position from the top
+
+		// if we've scrolled more than the navigation, change its position to fixed to stick to top, otherwise change it back to relative
+		if (scroll_top > sticky_navigation_offset_top) {
+			my_nav.addClass('stick');
+		} else {
+			my_nav.removeClass('stick');
+		}
+	};
+
+	var initio_parallax_animation = function () {
+		$('.parallax').each(function (i, obj) {
+			var speed = $(this).attr('parallax-speed');
+			if (speed) {
+				var background_pos = '-' + (window.pageYOffset / speed) + "px";
+				$(this).css('background-position', 'center ' + background_pos);
+			}
+		});
+	}
+
+	// run our function on load
+	sticky_navigation();
+
+	// and run it again every time you scroll
+	$(window).scroll(function () {
+		sticky_navigation();
+		initio_parallax_animation();
+	});
+
+
+	// // This part is for smooth scroll function
+	// // Add smooth scrolling to all links
+	// $("a").on('click', function (event) {
+
+	// 	// Make sure this.hash has a value before overriding default behavior
+	// 	if (this.hash !== "") {
+	// 		// Prevent default anchor click behavior
+	// 		event.preventDefault();
+
+	// 		// Store hash
+	// 		var hash = this.hash;
+
+	// 		// Using jQuery's animate() method to add smooth page scroll
+	// 		// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+	// 		$('html, body').animate({
+	// 			scrollTop: $(hash).offset().top
+	// 		}, 1000, function () {
+
+	// 			// Add hash (#) to URL when done scrolling (default click behavior)
+	// 			window.location.hash = hash;
+	// 		});
+	// 		return false;
+	// 	} // End if
+	// });
+
+
+	// // This part is for smooth scroll function
+	// // Add smooth scrolling to all links
+	// $("a").on('click', function (event) {
+
+	// 	// Make sure this.hash has a value before overriding default behavior
+	// 	if (this.hash !== "") {
+	// 		// Prevent default anchor click behavior
+	// 		event.preventDefault();
+
+	// 		// Store hash
+	// 		var hash = this.hash;
+
+	// 		// Using jQuery's animate() method to add smooth page scroll
+	// 		// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+	// 		$('html, body').animate({
+	// 			scrollTop: $(hash).offset().top - 50
+	// 		}, Math.abs(window.scrollY - $(this.hash).offset().top) * 0.7, function () {
+
+	// 			// Add hash (#) to URL when done scrolling (default click behavior)
+	// 			window.location.hash = hash;
+	// 		});
+	// 		return false;
+	// 	} // End if
+	// });
+
+
+
+	// $('.smooth-goto').on('click', function () {
+	// 	$('html, body').animate({ scrollTop: $(this.hash).offset().top - 50 }, 500);
+	// 	return false;
+	// });
+
+	$('.smooth-goto').on('click', function () {
+		$('html, body').animate({ scrollTop: $(this.hash).offset().top - 50 },
+			Math.abs(window.scrollY - $(this.hash).offset().top) * 0.7);
+		return false;
+	});
+
+	// $('#carousel-example-generic').carousel();
+	// $('#carousel-example-2').carousel();
+	// 4s for new slide in carousel
+	$('.carousel').carousel({
+		interval: 4000
+	})
+});
