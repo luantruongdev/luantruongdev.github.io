271a48a08ce9b6647a26878e13a49ba4b29decdc
luantruongdev <truongnhatluan95@gmail.com>
2021-01-16 13:40:29 +0100

    modify diverse

--- assets/js/template.js
+++ assets/js/template.js
@@ -1,3 +1,4 @@
+// 	jQuery script
 jQuery(document).ready(function ($) {
 
 	var my_nav = $('.navbar-sticky');
@@ -107,4 +108,82 @@ jQuery(document).ready(function ($) {
 	// 	interval: 4000
 	// })
 
+	// $("#btnShowHide").click(function () {
+	// 	$("#divShowHide").toggle();
+	// });
+
+	// $("#btnPassport").click(function () {
+	// 	if ($(this).val() == "Yes") {
+	// 		$("#dvPassport").show();
+	// 		$(this).val("No");
+	// 	} else {
+	// 		$("#dvPassport").hide();
+	// 		$(this).val("Yes");
+	// 	}
+	// });
+
 });
+
+// js script
+// button show hide
+function btnShowHide(btnID, divID, msg) {
+	var divID_rcv = document.getElementById(divID);
+	var btnID_rcv = document.getElementById(btnID);
+	if (divID_rcv.style.display == "none") {
+		divID_rcv.style.display = "block";
+		btnID_rcv.innerHTML = "Hide " + msg;
+	} else {
+		divID_rcv.style.display = "none";
+		btnID_rcv.innerHTML = "Show " + msg;
+	}
+}
+
+// button scroll to top
+const backToTopButton = document.querySelector("#back-to-top-btn");
+
+window.addEventListener("scroll", scrollFunction);
+
+function scrollFunction() {
+	if (window.pageYOffset > 300) { // Show backToTopButton
+		if (!backToTopButton.classList.contains("btnEntrance")) {
+			backToTopButton.classList.remove("btnExit");
+			backToTopButton.classList.add("btnEntrance");
+			backToTopButton.style.display = "block";
+		}
+	}
+	else { // Hide backToTopButton
+		if (backToTopButton.classList.contains("btnEntrance")) {
+			backToTopButton.classList.remove("btnEntrance");
+			backToTopButton.classList.add("btnExit");
+			setTimeout(function () {
+				backToTopButton.style.display = "none";
+			}, 250);
+		}
+	}
+}
+
+backToTopButton.addEventListener("click", smoothScrollBackToTop);
+
+function smoothScrollBackToTop() {
+	const targetPosition = 0;
+	const startPosition = window.pageYOffset;
+	const distance = targetPosition - startPosition;
+	const duration = 750;
+	let start = null;
+
+	window.requestAnimationFrame(step);
+
+	function step(timestamp) {
+		if (!start) start = timestamp;
+		const progress = timestamp - start;
+		window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+		if (progress < duration) window.requestAnimationFrame(step);
+	}
+}
+
+function easeInOutCubic(t, b, c, d) {
+	t /= d / 2;
+	if (t < 1) return c / 2 * t * t * t + b;
+	t -= 2;
+	return c / 2 * (t * t * t + 2) + b;
+};
